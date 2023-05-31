b00a2fe17508acf8f514add4be83a22956675666
Kevin IMT'21 <kchristian03@student.ciputra.ac.id>
2023-05-31 10:49:01 +0700

    logo

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -18,8 +18,8 @@
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
                 <a href="/">
-                    <img src="/assets/image/logo.svg">
-{{--                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />--}}
+{{--                    <img src="/assets/image/logo.svg">--}}
+                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
                 </a>
             </div>
 
