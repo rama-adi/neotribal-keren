da2ef21f99c8841cc6cdf05741fa1a030553e18d
rama-adi <17980909+rama-adi@users.noreply.github.com>
2023-05-31 14:03:38 +0700

    ai

--- app/Http/Livewire/TribalXAi.php
+++ app/Http/Livewire/TribalXAi.php
@@ -3,9 +3,79 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use OpenAI\Client;
 
 class TribalXAi extends Component
 {
+    public $history = [];
+
+    public $input = "";
+
+    public function mount()
+    {
+        $this->history[] = [
+            'role' => 'system',
+            'message' => implode(" ", [
+                'TribalX is an ai assistant specializing in Indonesian hidden gems.',
+                'Assistant will only respond with the data from the given context',
+                'and will not be able to answer questions outside of the context.',
+                'the ai is interacting with the user ' . auth()->user()->name,
+            ])
+        ];
+
+
+    }
+
+    public function ask()
+    {
+        $openAI = app(Client::class);
+        $pinecone = app(\Probots\Pinecone\Client::class);
+        $embedding = $openAI
+            ->embeddings()
+            ->create([
+                'model' => 'text-embedding-ada-002',
+                'input' => $this->input
+            ]);
+
+        $vectorFetch = $pinecone
+            ->index('neotribal')
+            ->vectors()
+            ->query(
+                vector: $embedding->embeddings[0]->embedding,
+                topK: 2
+            );
+
+        $responses = [];
+        foreach ($vectorFetch->json('matches') as $match) {
+            [$model, $id] = explode('-', $match['id']);
+
+            $model = match ($model) {
+                'location' => \App\Models\Location::find($id),
+                'locationstar' => \App\Models\LocationStar::find($id),
+            };
+
+            $responses[] = $model
+                ->find((int)$id)
+                ->description;
+        }
+
+
+        $this->history[] = [
+            'role' => 'user',
+            'message' => implode(" ", [
+                'question: ' . $this->input,
+                'Context: ' . implode(" ", $responses)
+            ])
+        ];
+
+        $response = $openAI->chat()->create([
+            'model' => 'gpt-3.5-turbo',
+            'messages' => $this->history,
+        ]);
+
+        dd($response);
+    }
+
     public function render()
     {
         return view('livewire.tribal-x-ai');
--- resources/views/livewire/tribal-x-ai.blade.php
+++ resources/views/livewire/tribal-x-ai.blade.php
@@ -1,3 +1,6 @@
-<div>
-    {{-- In work, do what you enjoy. --}}
+<div class="max-w-3xl mx-auto">
+    <form wire:submit.prevent="ask">
+        <x-text-input  wire:model="input"/>
+        <x-primary-button type="submit">Submit</x-primary-button>
+    </form>
 </div>
