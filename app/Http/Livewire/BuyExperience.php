edaa73bb3cb3c14f3dbeb508d1d158e15141162a
rama-adi <17980909+rama-adi@users.noreply.github.com>
2023-05-31 10:22:34 +0700

    fix buy transaction

--- app/Http/Livewire/BuyExperience.php
+++ app/Http/Livewire/BuyExperience.php
@@ -3,6 +3,8 @@
 namespace App\Http\Livewire;
 
 use App\Models\Location;
+use App\Models\Transaction;
+use Illuminate\Support\Str;
 use Livewire\Component;
 
 class BuyExperience extends Component
@@ -31,11 +33,30 @@ class BuyExperience extends Component
 
     public function buyExperience()
     {
-        auth()->user()->locations()->attach($this->selectedExperience->id);
-        $this->unsoldExperiences = Location::whereDoesntHave('users', function ($query) {
-            $query->where('user_id', auth()->id());
-        })->get();
-        $this->page = 'listing';
+
+        if (auth()->user()->coins >= $this->selectedExperience->coins) {
+
+            \DB::transaction(function () {
+                auth()->user()->locations()->attach($this->selectedExperience->id);
+                auth()->user()->decrement('coins', $this->selectedExperience->coins);
+                Transaction::create([
+                    'user_id' => auth()->id(),
+                    'code' => auth()->id() . "-AUTOCOMPLETE-EXPROBOT-" . strtoupper(Str::random(10)),
+                    'amount' => $this->selectedExperience->coins,
+                    'description' => 'Bought ' . $this->selectedExperience->name . ' experience (NO APPROVAL NEEDED)',
+                    'status' => 'completed'
+                ]);
+            });
+
+
+            $this->unsoldExperiences = Location::whereDoesntHave('users', function ($query) {
+                $query->where('user_id', auth()->id());
+            })->get();
+
+            $this->page = 'listing';
+        }
+
+
     }
 
 
